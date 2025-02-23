8f8348ffc11ac2a9f4b773ad42d6d4bc9b27b751
JamesVerhel02 <quizonjamesverhel_bsit@plmun.edu.ph>
2025-02-23 19:36:46 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,12 +1,12 @@
 grade = int(input("Enter your score: "))
 
 if grade <= 55:
-   print("Your Grade is: F" )
+print("Your Grade is: F" )
 elif grade == 60 or grade <= 65:
-     print("Your Grade is: D" )
+print("Your Grade is: D" )
 elif grade <= 70 or grade <= 75:
-     print("Your Grade is: C" )
+print("Your Grade is: C" )
 elif grade <= 80 or grade <= 85:
-     print("Your Grade is: B" )
+print("Your Grade is: B" )
 elif grade <= 90:
-     print("Your Grade is: A" )
+print("Your Grade is: A" )
